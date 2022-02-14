70456bc172e374dd96e5101522a8e3fbc1ef9013
Suvrodeep <81421963+SynTax19@users.noreply.github.com>
2022-02-15 00:20:17 +0530

    Final Done [2]

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,7 +3,10 @@ import loading from "./loading1.gif";
 
 const Spinner = () => {
   return (
-    <div className="text-center">
+    <div
+      className="text-center"
+      style={{ display: "flex", justifyContent: "center" }}
+    >
       <img className="my-3" src={loading} alt="loading" />
     </div>
   );
